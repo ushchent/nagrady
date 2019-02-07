c626253109fe43d04a13f3a648607717cbce11e3
ushchent <ushchent@ushchent-nt.ushchent>
2019-02-07 17:03:34 +0300

    Проверка BrowserRouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Info from "./Info";
 import "./App.css";
 import Header from "./Header";
@@ -12,12 +12,12 @@ const App = () => {
 	return (
     <div>
         {/*   <YMInitializer accounts={[26845122]} /> */}
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
 		<Header />
 		<Switch>
-            <Route exact path="/" />
-            <Route path="/nagrady" component={ Nagrady } />
-            <Route path="/data" component={ Data } />
+            <Route exact path={process.env.PUBLIC_URL + '/'} />
+            <Route path={process.env.PUBLIC_URL + "/nagrady"} component={ Nagrady } />
+            <Route path={process.env.PUBLIC_URL + "/data"} component={ Data } />
             <Route path="/info" component={ Info } />
             <Route render={() => <h1 className="page_title">Старонка ня знойдзена</h1>} />
 		</Switch>
